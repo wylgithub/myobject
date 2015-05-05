2d1ff678de0154a2c66322d8fab262ded7df0855
wylgithub <git@github.com>
2015-05-05 13:15:37 +0800

    系统首页设计:   添加一览信息轮播效果

--- myobject/views.py
+++ myobject/views.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*-
 import datetime
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render_to_response, render
+from income.models import Income, Expend, Lend, Borrow
+from plan.models import Monthly, Yearly
+from user_account.models import User
+from utility.base_view import get_list_params
+
 
 
 __author__ = 'wyl'
@@ -13,9 +19,50 @@ def index_view(request):
     """
     首页View
     """
-    # 如果是影院账户，则跳转到专用画面
-    login_user = request.user
+    # 用户一览queryset
+    queryset = User.objects.filter(is_superuser=False).exclude(is_active=False)
+    params = get_list_params(request)
+
+    # 收入信息一览
+    income_queryset = Income.objects.filter().exclude(delete_flg=True)
+    income_params = get_list_params(request)
+
+    # 获取收入信息的queryset
+    expend_queryset = Expend.objects.filter().exclude(delete_flg=True)
+    expend_params = get_list_params(request)
+
+    # 借出queryset
+    lend_queryset = Lend.objects.filter().exclude(delete_flg=True)
+
+    # 排序
+    lend_params = get_list_params(request)
+
+    borrow_queryset = Borrow.objects.filter().exclude(delete_flg=True)
+    # 排序
+    borrow_params = get_list_params(request)
+
+
+    month_queryset = Monthly.objects.filter().exclude(delete_flg=True)
+    month_params = get_list_params(request)
+
+    # 获取收入信息的queryset
+    year_queryset = Yearly.objects.filter().exclude(delete_flg=True)
+    year_params = get_list_params(request)
 
     return render(request, "index/index.html", {
-        'user': login_user
+        # 首页一览用户信息return开始
+        "users": queryset[params['from']:params['to']],
+        # 首页一览用户信息return结束
+        # 收入信息
+        'incomes': income_queryset[income_params['from']: income_params['to']],
+        # 支出信息
+        'expends': expend_queryset[expend_params['from']: expend_params['to']],
+        # 借出
+        'lends': lend_queryset[lend_params['from']: lend_params['to']],
+        # 借入
+        'borrows': borrow_queryset[borrow_params['from']: borrow_params['to']],
+        # 月收入计划
+        'months': month_queryset[month_params['from']: month_params['to']],
+        # 年收入计划
+        'years': year_queryset[year_params['from']: year_params['to']],
     })
